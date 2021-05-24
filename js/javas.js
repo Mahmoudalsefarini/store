9b35b983c5fa24d2b60295bbf525cc0c7bba3829
Mahmoudalsefarini <mahmoudalsafarini@gmail.com>
2021-05-24 08:21:08 +0300

    store

---
+++ js/javas.js
@@ -0,0 +1,9 @@
+let userName = prompt ('Whats your name ?')
+
+let userAge = prompt ('what is your age ?')
+
+let userGender = prompt('what is your gender ?')
+
+let favStore = prompt('what is your fav store ?')
+
+alert('Your Name is '+ userName + 'and your age is ' + userAge + 'your gender is ' + userGender + 'your fav store is ' + favStore );
